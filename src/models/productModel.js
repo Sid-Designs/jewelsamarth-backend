0c51d86ea9e4372ba2ae246ade7da42373fa6187
Sid-Designs <siddheshmhaskar05@gmail.com>
2025-02-22 16:05:31 +0530

    Add Product With Cloudinary

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -16,7 +16,7 @@ const productSchema = new Schema(
     },
     gender: {
       type: String,
-      enum: ["male", "female", "unisex"],
+      enum: ["Men", "Women", "Unisex"],
       required: [true, "Gender is required"],
     },
     regprice: {
